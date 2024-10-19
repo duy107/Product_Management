7dd267cc166aa05d252b10f6af93b11c94a6481e
duy107 <trinhduy107@gmail.com>
2024-10-19 20:14:56 +0700

    19/10

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://duytrinhcong107:SD3vKD6hP0QX4M9h@cluster0.cq7eh.mongodb.net/ProductManagement");
         console.log("Connect success");
     } catch (error) {
         console.log("Connect ERROR");
